cfcfefa0707883b9186d5d2ebcf81f18823ff156
daniel <Daniel@SilverEngineered.com>
2019-04-09 13:04:24 -0400

    removed mnist dataset

--- preProcess.py
+++ preProcess.py
@@ -1,10 +1,12 @@
 import numpy as np
-
-
-def import_images():
-    pass
+import os
+import gzip
+def import_images(datapath="./",MNIST_TRAIN = 'train-images-idx3-ubyte.gz'):
+    f = gzip.open(MNIST_TRAIN,'r')
+    x=f.read(16)
+    print(x)
 def pad_and_resize():
     pass
 def synthetic_degredation():
     pass
-
+import_images()
