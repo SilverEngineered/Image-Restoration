232fe440006517642a3ac5cdfe2d1cbb16113f5b
daniel <Daniel@SilverEngineered.com>
2019-04-09 13:38:08 -0400

    added steps to pickle mnist

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Image-Restoration
+
+
+##Code for MNIST Pickle Download from https://github.com/hsjeong5/MNIST-for-Numpy
+Note: In order for adapted code to pickle MNIST, download all mnist files from http://yann.lecun.com/exdb/mnist/
--- preProcess.py
+++ preProcess.py
@@ -1,12 +1,20 @@
 import numpy as np
 import os
 import gzip
-def import_images(datapath="./",MNIST_TRAIN = 'train-images-idx3-ubyte.gz'):
-    f = gzip.open(MNIST_TRAIN,'r')
-    x=f.read(16)
-    print(x)
+import pickle
+import cv2
+def import_images(datapath="../data/mnist",MNIST_TRAIN = 'mnist.pkl'):
+    full_path = os.path.join(datapath,MNIST_TRAIN)
+    with open(full_path,'rb') as f:
+        mnist = pickle.load(f)["training_images"]
+    print("Done Loading")
+    mnist = np.reshape(mnist,[-1,28,28])
+    return mnist
 def pad_and_resize():
     pass
 def synthetic_degredation():
     pass
-import_images()
+def save_image(img):
+    cv2.imwrite("img_0.jpg",img)
+img = import_images()[0]
+save_image(img)
