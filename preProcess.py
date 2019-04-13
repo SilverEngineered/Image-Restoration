60a56b80d68e4ae81e846f24bbff467cef8c268b
SilverEngineered <Daniel@SilverEngineered.com>
2019-04-12 20:51:07 -0400

    filled in preprocessing functions

--- preProcess.py
+++ preProcess.py
@@ -23,7 +23,7 @@ def import_images(datapath="../data/mnist",MNIST_filename = 'mnist.pkl'):
     print("Done Loading")
     mnist = np.reshape(mnist,[-1,28,28])
     return mnist
-def scale_images(images,scaling=(0,1))
+def scale_images(images,scaling=[0,1],dtype=np.float32)
     """  Scale an array of images to the specified scaling range
 
          inputs:
@@ -39,8 +39,11 @@ def scale_images(images,scaling=(0,1))
          outputs:
              numpy array of images scaled from min to max
     """
-    pass
-def pad_images(images,padding=(0,0)):
+    min_data, max_data = [float(np.min(images)), float(np.max(images))]
+    min_scale, max_scale = [float(scaling[0]), float(scaling[1])]
+    data = ((max_scale - min_scale) * (images - min_data) / (max_data - min_data)) + min_scale
+    return data.astype(dtype)
+def pad_images(images,pad_shape=(0,0)):
     """  Pads images with 0s to fit a desired shape
 
          inputs:
@@ -56,7 +59,10 @@ def pad_images(images,padding=(0,0)):
          outputs:
              padded images of size (x,i + 2 * row,j + 2 * col) 
     """
-    pass
+    padded_images = []
+    for i in range(images.shape[0]):
+        padded_images.append[np.pad(images,pad_shape,'constant')]
+    return np.array(padded_images)
 def synthetic_degredation(images,deg_function):
     """  Applies a degredation to all images in image_array
          by applying a degredation function
@@ -71,7 +77,7 @@ def synthetic_degredation(images,deg_function):
         outputs:
             degredated images -> (x,i,j)
     """
-    pass
+    return np.array(list(list(images).map(lambda x: deg_function(x)))
 def save_image(img,img_path="img_0.jpg"):
     """  Save output image to img_path
 
