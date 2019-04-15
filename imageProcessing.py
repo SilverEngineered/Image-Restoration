9c4f3e508357517dea9d450853985b93d57eabf8
CooperL <cooper.loughlin@gmail.com>
2019-04-15 17:11:13 -0400

    tested image processing functions

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,43 +1,63 @@
 import numpy as np
+from scipy.stats import norm
 
-def deg_function(x,filt_param,filt_size,noise_power):
+def degrade_image(x,sigma,N_filt,noise_power):
     """ Performs artificial degradation to a single image.
         The image is first blurred then Gaussian noise is added.
-
+        
         inputs:
             x           -> input image np array (zero padded)
-            filt_param  -> parameter determining the size of the blur filter
-            filt_size   -> size of square filter support
+            sigma       -> parameter determining the size of the blur filter
+            N_filt      -> size of square filter support
             noise_power -> variance of additive Gaussian noise
         outputs:
             y -> degraded output image
     """
-    pass
-def filter(x,filt_param,filt_size,filt_type="gauss"):
+    x_filt = filter_image(x,sigma,N_filt)
+    return add_noise(x_filt,noise_power)
+
+def filter_image(x,sigma,N_filt,filt_type="gauss"):
     """ Applies linear filter.
 
         inputs:
-            x          -> input image np array (zero padded)
-            filt_param -> parameter determining the size of the blur filter
-            filt_size  -> size of square filter support
-            filt_type  -> filter shape (default=gauss)
+            x         -> input image np array (zero padded)
+            sigma     -> parameter determining the size of the blur filter
+            Nfilt     -> size of square filter support
+            filt_type -> filter shape (default=gauss)
         outputs:
-            y -> filtered image
+            y -> filtered image real part only
     """
-    pass
-def inverse_filter(x,filt_param,filt_size,filt_type="gauss",lam=0):
+    H = gauss_filter(sigma,N_filt)
+    N_im = x.shape
+    N_pad1 = N_im[0]-N_filt
+    N_pad2 = N_im[1]-N_filt
+    Hpad = np.pad(H,((0,N_pad1),(0,N_pad2)),'constant')
+    y = np.fft.ifft2(np.fft.fft2(Hpad)*np.fft.fft2(x))
+    return np.real(y)
+ 
+def inverse_filter_image(x,sigma,N_filt,filt_type="gauss",lam=0):
     """ Applies linear inverse filter with regularization.
 
         inputs:
-            x          -> input image np array (zero padded)
-            filt_param -> parameter determining the size of the blur filter
-            filt_size  -> size of square filter support
-            filt_type  -> filter shape (default=gauss)
-            lam        -> regularization parameter
+            x         -> input image np array (zero padded)
+            sigma     -> parameter determining the size of the blur filter
+            N_filt    -> size of square filter support
+            filt_type -> filter shape (default=gauss)
+            lam       -> regularization parameter
         outputs:
             y -> filtered image
     """
-    pass
+    
+    H = gauss_filter(sigma,N_filt)
+    N_im = x.shape
+    N_pad1 = N_im[0]-N_filt
+    N_pad2 = N_im[1]-N_filt
+    Hpad = np.pad(H,((0,N_pad1),(0,N_pad2)),'constant')
+    Hfft = np.fft.fft2(Hpad)
+    Hffti = 1.0 / (np.add(Hfft,1.0*lam))
+    y = np.fft.ifft2(Hffti*np.fft.fft2(x))
+    return np.real(y)
+
 def add_noise(x,noise_power):
     """ Adds white Gaussian noise to image.
 
@@ -47,16 +67,31 @@ def add_noise(x,noise_power):
         outputs:
             y -> noisy image
     """
-    size_x = x.shape()
+    size_x = x.shape
     noise = np.random.normal(0,np.sqrt(noise_power),size_x)
     return np.add(x,noise)
-def gauss_filter(filt_param,filt_size):
+
+def gauss_filter(sigma,N):
     """ Returns Gaussian filter impulse response as np array.
 
         inputs:
-            filt_param -> parameter determining the size of the blur filter
-            filt_size  -> size of square filter support
+            sigma -> parameter determining the size of the blur filter
+            N     -> size of square filter support
         outputs:
-            h -> Gaussian filter impulse response (filt_size-by-filt_size)
+            H -> Gaussian filter impulse response (filt_size-by-filt_size)
     """
-    pass
+    if N%2 == 0:
+        raise ValueError('filt_size should be odd')
+        return
+
+    N2 = (N-1)/2
+    x = np.linspace(-N2,N2,N)
+    h = norm.pdf(x/sigma)
+
+    return np.outer(h,h)
+
+
+
+
+
+
--- ip_test.py
+++ ip_test.py
@@ -1,7 +1,42 @@
 import numpy as np
 import preProcess as pp
 import imageProcessing as ip
-import matplotlib.pyplot as plt
 
 img = pp.import_images()[0]
-imgplot = plt.imshow(img)
+
+im_size = (512,512)
+im0 = np.zeros(im_size)
+print im0
+noise_power = 1
+noise = ip.add_noise(im0,noise_power)
+print noise
+
+noise_scale = pp.scale_images(noise,scaling=[0,255],dtype=np.uint8)
+pp.save_image(noise_scale,"test_images/noise_512.jpg")
+
+sigma = 5
+Nfilt = 5*sigma
+print 'filter size: ',Nfilt
+H = ip.gauss_filter(sigma,Nfilt)
+H_scale = pp.scale_images(H,scaling=[0,255],dtype=np.uint8)
+pp.save_image(H_scale,"test_images/H_%.2f.jpg" % sigma)
+
+Nfilt2 = (Nfilt-1)/2
+print 'Nfilt2: ',Nfilt2
+
+noise_pad = np.pad(noise,(Nfilt2,Nfilt2),'constant')
+print 'upadded size: ',noise.shape
+print 'padded size: ',noise_pad.shape
+
+y = ip.filter_image(noise_pad,sigma,Nfilt)
+y_scale = pp.scale_images(y,scaling=[0,255],dtype=np.uint8)
+pp.save_image(y_scale,'test_images/y.jpg')
+
+lam = 1
+yi = ip.inverse_filter_image(y,sigma,Nfilt,lam=lam)
+yi_scale = pp.scale_images(yi,scaling=[0,255],dtype=np.uint8)
+pp.save_image(yi_scale,'test_images/yi_%.2f.jpg' % lam)
+
+deg_img = ip.degrade_image(noise_pad,sigma,Nfilt,noise_power)
+deg_scale = pp.scale_images(deg_img,scaling=[0,255],dtype=np.uint8)
+pp.save_image(deg_scale,'test_images/deg_img_%.2f.jpg' % noise_power)
