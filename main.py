77f3be5610f7b6ee4646be871982d39048755806
CooperL <cooper.loughlin@gmail.com>
2019-04-17 13:16:03 -0400

    changed output analysis file name to include num partitions and noise power

--- main.py
+++ main.py
@@ -21,15 +21,14 @@ def npToRDD(path,sc,N,num_images):
     for i in range(num_images):
         img_with_index.append((i,imgs[i]))
     return sc.parallelize(img_with_index).partitionBy(numPartitions=N)
-def writeOutput(y_path,N,lam,time_elps,filename):
+def writeOutput(y_path,N,best_lam,time_elps,rss_list,lam_list,filename):
     """  Outputs analysis information to specified filename as csv file """
     with open(filename,'w') as csv_file:
         writer = csv.writer(csv_file,delimiter=',',quotechar='"')
-        writer.writerow(["YRDD Input File", "Num Partitions", "Best Lambda", "Time Taken"])
-        data_row = [y_path,str(N),str(lam),str(time_elps)]
+        writer.writerow(["YRDD Input File", "Num Partitions", "Best Lambda", "Time Taken","Lambda List", "RSS"])
+        data_row = [y_path,str(N),str(best_lam),str(time_elps),str(lam_list),str(rss_list)]
         writer.writerow(data_row)
         writer.writerow([])
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--x_data_path", default='../data/mnist/mnist.npy')    
@@ -37,11 +36,14 @@ if __name__ == "__main__":
     parser.add_argument("--lambdas", default=[2* i for i in range(10)])    
     parser.add_argument("--k", type = int,default=10)
     parser.add_argument("--num_partitions", default=10)
-    parser.add_argument("--data_out", default="analysis.csv")
     parser.add_argument("--save_image", default=False)
     parser.add_argument("--num_images", type=int, default=300)
     parser.add_argument("--img_index_to_save", default=0)
     args = parser.parse_args()
+
+    noise_power_str = args.y_data_path.split('/')[-1]
+    noise_power_str = noise_power_str[0:3]
+    data_out = 'output_analysis/analysis_N_'+str(args.num_partitions)+'_noise_'+noise_power_str+'.csv'
 
     sc = SparkContext()
 
@@ -57,10 +59,8 @@ if __name__ == "__main__":
 
     time_elps = time.time() - start
     
-    print 'rss_vals: ',rss_vals
-    print ' lambdas: ',args.lambdas
-    
-    writeOutput(args.y_data_path,args.num_partitions,best_lam,time_elps,args.data_out)
+    writeOutput(args.y_data_path,args.num_partitions,best_lam,time_elps, 
+                args.lambdas,rss_vals,data_out)
     if args.save_image:
         img = pp.scale_images(Z.collect(),scaling=[0,255])[0]
         pp.save_image(img)
